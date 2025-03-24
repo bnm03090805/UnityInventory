bb3a4c37a1ca7b043867fa5b62105ccd08be978b
bnm03090805 <bnm03090805@gmail.com>
2025-03-24 14:20:23 +0900

    [feat] 캐릭터 스크립트 작성

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float Atk { get; set; } = 35f;
+    public float Def { get; set; } = 40f;
+    public float HP { get; set; } = 100f;
+    public float Critical { get; set; } = 25f;
+    public float Gold { get; set; } = 2000f;
+
+    public float AtkModifier { get; set; }
+    public float DefModifier { get; set; }
+    public float HPModifier { get; set; }
+    public float CriticalModifier {  get; set; }
+
     void Start()
     {
         
